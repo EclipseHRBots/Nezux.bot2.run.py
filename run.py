b691903baabe50514a024fa4b9259b02c402fcb1
BOT Nezux <zzuxx81@gmail.com>
2024-12-15 00:24:03 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from threading import Thread
+from highrise.__main__ import *
+import time
+
+
+class WebServer():
+
+  def __init__(self):
+    self.app = Flask(__name__)
+
+    @self.app.route('/')
+    def index() -> str:
+      return "Funcionando"
+
+  def run(self) -> None:
+    self.app.run(host='0.0.0.0', port=8080)
+
+  def keep_alive(self):
+    t = Thread(target=self.run)
+    t.start()
+
+
+class RunBot():
+  room_id = "675c2b22631d3144b5055bbb"
+  bot_token = "497761234868f1f869f8ae5400f668efaffa3ccbbcf4b5ef16d2f7e7991ab2b7"
+  bot_file = "main"
+  bot_class = "Bot"
+
+  def __init__(self) -> None:
+    self.definitions = [
+        BotDefinition(
+            getattr(import_module(self.bot_file), self.bot_class)(),
+            self.room_id, self.bot_token)
+    ]  # More BotDefinition classes can be added to the definitions list
+
+  def run_loop(self) -> None:
+    while True:
+      try:
+        arun(main(self.definitions))
+
+      except Exception as e:
+        print("Error: ", e)
+        time.sleep(5)
+
+if __name__ == "__main__":
+  WebServer().keep_alive()
+
+  RunBot().run_loop()
